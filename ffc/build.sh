2e2819dc50097ee5f018cbc74755de81e40e3776
Yimin <GaZ3ll3@users.noreply.github.com>
2015-07-15 22:07:47 -0500

    Update build.sh
    fix the linking for clang

--- ffc/build.sh
+++ ffc/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ `uname` == Darwin ]; then
+  MACOSX_VERSION_MIN=10.8
+  export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
+fi
+
 $PYTHON setup.py install --prefix=$PREFIX
 
 # Add more build steps here, if they are necessary.
